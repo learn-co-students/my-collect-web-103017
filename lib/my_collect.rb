529d1dbd4039cbc64c259b25c20caafca60fa86b
github username <github email address>
2017-10-12 17:12:21 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+  i = 0
+  output = []
+  while i < collection.length
+    yield(collection[i])
+    output << yield(collection[i])
+    i += 1
+  end
+  output
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
 
+# i want to yield a collection, and after fuxing with it return the fuxed with thing back
