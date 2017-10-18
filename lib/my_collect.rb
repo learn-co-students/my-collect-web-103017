5de15a999cf0d0692797576943faceccabd7e34c
Silvia Chung <schung97@gmail.com>
2017-10-18 02:09:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(collection)
+  i = 0
+  revised = []
+  while i < collection.length
+    revised.push(yield collection[i])
+      i += 1
+  end
+  revised
+end
 
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(students) do |student|
+   student.split(' ').first
+end
+
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+
+my_collect(languages) do |language|
+  language.upcase
+end
