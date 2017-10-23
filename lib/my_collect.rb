bfa39a30f5330852d980027e014bc79d7c7e6e77
Katerina  Papacostas <katpapacostas@gmail.com>
2017-10-23 02:03:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  return 0 if array.length == 0
 
+  if block_given?
+    times = array.length - 1
+    i = 0
+    new_array = []
+      while i <= times
+        new_array << (yield array[i])
+        i += 1
+      end
 
+  else
+    "No block was given!"
+  end
+
+  new_array
+end
